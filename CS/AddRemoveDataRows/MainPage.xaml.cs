195be52ef80f5bf94ddf5752fee01fc4ee34e2e8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AddRemoveDataRows/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace AddRemoveDataRows {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = new ProductList();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e) {
+            (grid.ItemsSource as ProductList).Add(new Product() { ProductName = "(Empty) Product Name" });
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+            if(grid.IsValidRowHandle(view.FocusedRowHandle))
+                view.DeleteRow(view.FocusedRowHandle);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to programmatically add or remove grid rows
+
+
+<p>This example shows how to programmatically add an empty row and delete a focused row. Since the grid is bound to a data source derived from the ObservableCollection(T) class, new rows can also be added using the New Item Row.</p>
+
+<br/>
+
+

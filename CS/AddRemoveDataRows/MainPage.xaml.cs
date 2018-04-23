2f13f9e29bf934a840fd27db9e3a45bc48053924
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AddRemoveDataRows/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace AddRemoveDataRows {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = new ProductList();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e) {
+            (grid.DataSource as ProductList).Add(new Product() { ProductName = "(Empty) Product Name" });
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+            if(grid.IsValidRowHandle(grid.View.FocusedRowHandle))
+                grid.View.DeleteRow(grid.View.FocusedRowHandle);
+        }
+    }
+}
---
+++ VB/AddRemoveDataRows/MainPage.xaml.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace AddRemoveDataRows
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = New ProductList()
+		End Sub
+
+		Private Sub AddButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			TryCast(grid.DataSource, ProductList).Add(New Product() With {.ProductName = "(Empty) Product Name"})
+		End Sub
+
+		Private Sub DeleteButton_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			If grid.IsValidRowHandle(grid.View.FocusedRowHandle) Then
+				grid.View.DeleteRow(grid.View.FocusedRowHandle)
+			End If
+		End Sub
+	End Class
+End Namespace
